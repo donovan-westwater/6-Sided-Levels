337e9498de396b37d925d432c56dd32efd24f80b
Donovan Westwater <donovan.westwater@gmail.com>
2020-01-03 17:00:29 -0800

    created the prototypes for the first levels of the game. Going to work on a level transition system next, and then some bounds checking for the rotations

---
+++ Assets/LevelExit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExit : MonoBehaviour
+{
+    //GOAL: This should incriment the current level count when touched by the player. In should also tell the level manager to load the next level
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
