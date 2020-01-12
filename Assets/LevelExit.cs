b069b5d59380dcbd478f247a3899190db738ef05
Donovan Westwater <donovan.westwater@gmail.com>
2020-01-12 00:29:32 -0500

    Level manager basics are done. Going to work on making bounds checking for going from rotate mode into 2d mode

--- Assets/LevelExit.cs
+++ Assets/LevelExit.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class LevelExit : MonoBehaviour
 {
     //GOAL: This should incriment the current level count when touched by the player. In should also tell the level manager to load the next level
+    //Current issue: Level loads as soon as contact is made! needs to only look for collsions when the player isnt rotating
     // Start is called before the first frame update
+    Level_Manager manager;
+    PlayerControl player;
     void Start()
     {
-        
+        manager = GameObject.Find("Level Manager").GetComponent<Level_Manager>();
+        player = GameObject.Find("Player").GetComponent<PlayerControl>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(!player.rotateMode) manager.nextLevel();
     }
 }
--- Assets/Level_Manager.cs
+++ Assets/Level_Manager.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Level_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Level Manager should keep be the one keeping the stats on player, not player itself
+    public static Level_Manager manager;
+    int currentLevel = 0;
+    int highestUnlockedLevel; //Dont use for now. This exists purely for making a level selecter in the future. 
+                              //The level manager infomation is the stuff that is going to be used for saving
+    void Awake()
     {
-        
+        if (manager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            manager = this;
+        }
+        else if (manager != this)
+        {
+            Destroy(gameObject);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void nextLevel()
     {
-        
+        currentLevel += 1;
+        SceneManager.LoadScene(currentLevel);
     }
 }
