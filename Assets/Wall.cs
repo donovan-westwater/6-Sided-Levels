d7d81099642519591e80799bdc51bfeead0db992
Skooz <gadzooks297@gmail.com>
2021-04-05 15:08:33 -0400

    merging

--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -41,7 +41,7 @@ public class Wall : MonoBehaviour
             if (r.collider == this.GetComponent<BoxCollider2D>())
             {
                 Physics2D.IgnoreCollision(Player.GetComponent<CircleCollider2D>(), this.GetComponent<BoxCollider2D>(), (Player.GetComponent<PlayerControl>().rotateMode && this.CompareTag("Wall")));
-                if(this.CompareTag("PhaseWall")) Physics2D.IgnoreCollision(Player.GetComponent<CircleCollider2D>(), this.GetComponent<BoxCollider2D>(), (!Player.GetComponent<PlayerControl>().rotateMode));
+                if (this.CompareTag("PhaseWall")) Physics2D.IgnoreCollision(Player.GetComponent<CircleCollider2D>(), this.GetComponent<BoxCollider2D>(), (!Player.GetComponent<PlayerControl>().rotateMode));
                 if (this.CompareTag("RotateWall")) Debug.Log(Player.GetComponent<PlayerControl>().rotateMode && !this.CompareTag("RotateWall"));
                 noHit = false;
             }
