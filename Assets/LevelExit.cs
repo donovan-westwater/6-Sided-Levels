61a09ccce1df5e4c32463a646a7b48ad316ce4e1
donovan-westwater <donovan.westwater@gmail.com>
2021-04-25 18:28:04 -0400

    Fixed level exit to only work when on same side as player. Fixed squishing issue by having it restart based on collision point distance. Planning to turn backgrounds white, turn on music, and do a quick playthrough of the game to see how it plays

--- Assets/LevelExit.cs
+++ Assets/LevelExit.cs
@@ -9,13 +9,44 @@ public class LevelExit : MonoBehaviour
     // Start is called before the first frame update
     public GameObject manager;
     public GameObject player;
+    Camera main;
     void Start()
     {
         manager = GameObject.Find("Level Manager");
         player = GameObject.Find("Player");
+        main = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
+    private void FixedUpdate()
+    {
+        bool noHit = true;
+        Vector3 dir = main.transform.position - this.transform.position;
+        RaycastHit ray;
+        bool checkhit = Physics.Raycast(main.transform.position, -(main.transform.position - player.transform.position), out ray, 100f, Physics.DefaultRaycastLayers);
+        float zlayer = ray.point.z * 0.5f; //Currently is causing the check to ignore walls by having layer be after wall
+        if (!checkhit) zlayer = 0.5f;
+        //End of new code
+        RaycastHit2D[] rays;
+        rays = Physics2D.RaycastAll(main.transform.position, -dir, 100f, Physics2D.DefaultRaycastLayers, -11, zlayer); //Orignally 0.5f
+        Debug.DrawLine(main.transform.position, main.transform.position - dir);
+        foreach (RaycastHit2D r in rays)
+        {
+            if (r.collider.gameObject.tag.Equals("Level"))
+            {
+                break;
+            }
+            if (r.collider == this.GetComponent<BoxCollider2D>())
+            {
+                Physics2D.IgnoreCollision(player.GetComponent<CircleCollider2D>(), this.GetComponent<CircleCollider2D>(), player.GetComponent<PlayerControl>().rotateMode);
+                noHit = false;
+            }
+        }
+        if (noHit)
+        {
+            Physics2D.IgnoreCollision(player.GetComponent<CircleCollider2D>(), this.GetComponent<CircleCollider2D>(), true);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(!player.GetComponent<PlayerControl>().rotateMode) manager.GetComponent<Level_Manager>().nextLevel();
