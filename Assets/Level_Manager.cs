d3e30d7d418864255964d0b5d68b5f24063ace12
Skooz <gadzooks297@gmail.com>
2021-04-25 15:55:38 -0400

    quick fix to level restart

--- Assets/Level_Manager.cs
+++ Assets/Level_Manager.cs
@@ -34,6 +34,6 @@ public class Level_Manager : MonoBehaviour
     }
     public void restartLevel()
     {
-        SceneManager.LoadScene(currentLevel);
+        SceneManager.LoadScene(currentLevel-1);
     }
 }
