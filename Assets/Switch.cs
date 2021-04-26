7006267c59ee01aff723b9b4084f4f5284fba369
donovan-westwater <donovan.westwater@gmail.com>
2021-04-25 21:44:05 -0400

    Changed all the scenes to have a white background. Fixed the switch and levelexit to work properly now

--- Assets/Switch.cs
+++ Assets/Switch.cs
@@ -9,14 +9,45 @@ public class Switch : MonoBehaviour
     public GameObject[] RotateWalls;
     public GameObject[] PhaseWalls;
     public GameObject[] Walls;
+    Camera main;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        main = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
 
     // Collision checks
+    private void FixedUpdate()
+    {
+        bool noHit = true;
+        Vector3 dir = main.transform.position - this.transform.position;
+        RaycastHit ray;
+        bool checkhit = Physics.Raycast(main.transform.position, -(main.transform.position - player.transform.position), out ray, 100f, Physics.DefaultRaycastLayers);
+        float zlayer = ray.point.z * 0.5f; //Currently is causing the check to ignore walls by having layer be after wall
+        if (!checkhit) zlayer = 0.5f;
+        //End of new code
+        RaycastHit2D[] rays;
+        rays = Physics2D.RaycastAll(main.transform.position, -dir, 100f, Physics2D.DefaultRaycastLayers, -11, zlayer); //Orignally 0.5f
+        Debug.DrawLine(main.transform.position, main.transform.position - dir);
+        foreach (RaycastHit2D r in rays)
+        {
+            if (r.collider.gameObject.tag.Equals("Level"))
+            {
+                break;
+            }
+            if (r.collider == this.GetComponent<CircleCollider2D>())
+            {
+                Physics2D.IgnoreCollision(player.GetComponent<CircleCollider2D>(), this.GetComponent<CircleCollider2D>(), player.GetComponent<PlayerControl>().rotateMode);
+                noHit = false;
+            }
+        }
+        if (noHit)
+        {
+            Physics2D.IgnoreCollision(player.GetComponent<CircleCollider2D>(), this.GetComponent<CircleCollider2D>(), true);
+        }
+    }
     // Cycle: RotateWall -> PhaseWall -> Wall. And repeat!
     private void OnTriggerEnter2D(Collider2D collision)
     {
